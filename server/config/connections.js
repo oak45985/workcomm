459597971438028ca7271ee10ed06db43a12eec7
oak45985 <oak45985@gmail.com>
2022-11-10 19:06:11 -0600

    deploy

--- server/config/connections.js
+++ server/config/connections.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/workcomm', 
     {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
     }
 );
 

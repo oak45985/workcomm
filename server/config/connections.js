c761454a78ca3e5bbbe1b8af74bee607b98a270c
oak45985 <oak45985@gmail.com>
2022-11-10 18:48:09 -0600

    updated connection

--- server/config/connections.js
+++ server/config/connections.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workcomm', 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/workcomm', 
     {
         useNewUrlParser: true,
         useUnifiedTopology: true

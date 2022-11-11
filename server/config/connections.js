23a91f96511147d28a0649f98bff773677b2c82e
oak45985 <oak45985@gmail.com>
2022-11-10 19:12:33 -0600

    redeploy

--- server/config/connections.js
+++ server/config/connections.js
@@ -4,8 +4,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/workcomm'
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
     }
 );
 
